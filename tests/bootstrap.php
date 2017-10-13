d304bce9776f1cb93c4a4498ebaaadd73f331d49
Kim Sandell <Kim.Sandell@kindredgroup.com>
2017-10-13 11:14:07 +0200

    Added phpunit test bootstrap

---
+++ tests/bootstrap.php
@@ -0,0 +1,18 @@
+<?php declare(strict_types=1);
+
+/**
+ * This file is part of the spin-framework
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ *
+ * @package  spin-framework
+ */
+
+use PHPUnit\Framework\TestCase;
+
+date_default_timezone_set('UTC');
+require __DIR__.'/../vendor/autoload.php';
+
+# Create Nofuzz application
+$app = new \Spin\Application( realpath(__DIR__) );
