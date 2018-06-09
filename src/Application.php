b4450072d0c22d1b5944f7c91924440ceb88d1c3
Kim Sandell <kimsandell73@gmail.com>
2018-06-10 00:25:50 +0200

    Version to 0.0.6

--- src/Application.php
+++ src/Application.php
@@ -22,7 +22,7 @@ use \Psr\Http\Message\Response;
 class Application extends AbstractBaseClass implements ApplicationInterface
 {
   /** @const      string          Application/Framework version */
-  const VERSION = '0.0.5';
+  const VERSION = '0.0.6';
 
   /** @var        string          Application Environment (from ENV vars) */
   protected $environment;
