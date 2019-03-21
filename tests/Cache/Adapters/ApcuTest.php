e873ecf22ded88da597cee9f0fd3fa9a95edf572
Kim Sandell <kimsandell73@gmail.com>
2019-03-21 06:50:57 +0100

    apcu test

--- tests/Cache/Adapters/ApcuTest.php
+++ tests/Cache/Adapters/ApcuTest.php
@@ -10,9 +10,19 @@ class ApcuTest extends TestCase
 {
   public function testTestAdapter()
   {
-    $obj = new Apcu();
+    try {
+      $obj = new Apcu();
+      $this->assertFalse( \is_null($obj) );
 
-    $this->assertFalse( \is_null($obj) );
+    } catch (\Exception $e) {
+
+      if ($e->getMessage() === 'Cache driver APCu not available') {
+        # APCU is not available, assume OK
+        $this->assertFalse( true );
+      }
+
+      throw $e;
+    }
   }
 
 }
