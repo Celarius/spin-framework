5d580a654dc113b2b368ee69293eef6b18f10734
Kim Sandell <kim@ewq.zone>
2020-06-10 15:37:33 +0200

    FILEINFO_MIME_TYPE fix

--- src/Helpers.php
+++ src/Helpers.php
@@ -647,7 +647,7 @@ if(!\function_exists('mime_content_type')) {
     if (\array_key_exists($ext, $mime_types)) {
       return $mime_types[$ext];
     } elseif (\function_exists('finfo_open')) {
-      $finfo = \finfo_open(FILEINFO_MIME);
+      $finfo = \finfo_open(\FILEINFO_MIME_TYPE);
       $mimetype = \finfo_file($finfo, $filename);
       \finfo_close($finfo);
 
